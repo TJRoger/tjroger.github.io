778cd454764ab6c0250119fd5f27f2168a85bc82
Roger Luo <tjrogertj@gmail.com>
2020-04-07 22:05:22 +0800

    add apps list to homepage

---
+++ apps/index.php
@@ -0,0 +1,14 @@
+<?php
+/***************************************************************************
+ * Copyright (c) 2020  The AppsMoto Authors
+ * ZingFront.com, Inc. All Rights Reserved
+ **************************************************************************/
+
+/**
+ * @file index.php
+ * @author rogerluo[tjrogertj@gmail.com]
+ * @date 2020-04-07 22:04:40
+ * @desc
+ */
+/* vim: set ts=4 sw=4 sts=4 tw=100 */
+echo "<h1>php test</h1>"
